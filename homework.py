e2dff994b8aec1a20aa92950458aab1bc82bb53a
Nataliya Rodikova <bachininang@gmail.com>
2022-09-24 16:01:36 +0500

    sprint7project3

--- homework.py
+++ homework.py
@@ -77,7 +77,7 @@ def get_api_answer(current_timestamp):
     преобразовав его из формата JSON к типам данных Python.
     """
     logger.info('Пытаемся отправить запрос')
-    timestamp = 1549962000
+    timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
     request_params = {
         'url': ENDPOINT,
